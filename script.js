db51b19d40bdb3ad8f2dbfa6a825bdfc6a273e70
Curt <spiegelhaltercurt@gmail.com>
2021-01-04 15:11:07 -0600

    Added some javascript

--- script.js
+++ script.js
@@ -16,14 +16,15 @@ $(document).ready(function() {
 
 
 function weather() {
+    var appID = "9542d4c6783a974140e65af253cf4a14";
+    
+    var city = $("#userCity").val()
     $.ajax({
-        url: `api.openweathermap.org/data/2.5/forecast/daily?q=${$("#userCity").val()}&cnt=5&appid=9542d4c6783a974140e65af253cf4a14
-        `,
+        url: "https://api.openweathermap.org/data/2.5/forecast/daily?q=" + city + "&cnt=5&appid=9542d4c6783a974140e65af253cf4a14",
         method: "GET"
+      }).then(function(response){
+          console.log(response)
       })
-      .then(response => response.json())
-      .then(data => console.log(data))
-        // console.log(response);
       
 
     }})
